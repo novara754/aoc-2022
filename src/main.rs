c5e914e34ee76b9f99b786b2fcec9908446ae6bb
vzwGrey <vzwgrey@gmail.com>
2022-12-08 15:16:47 +0100

    add thing to only run a single day

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{fmt::Display, time::Instant};
 
 use harness::PuzzleSolution;
 
@@ -12,29 +12,33 @@ mod day7;
 mod day8;
 mod harness;
 
+fn print_solution(day_num: usize, solution: impl PuzzleSolution<Output = impl Display>) {
+    let input = std::fs::read_to_string(format!("inputs/day{}.txt", day_num)).unwrap();
+
+    let (part1, part1_time) = {
+        let start = Instant::now();
+        let part1 = solution.part1(&input);
+        (part1, start.elapsed())
+    };
+    let (part2, part2_time) = {
+        let start = Instant::now();
+        let part2 = solution.part2(&input);
+        (part2, start.elapsed())
+    };
+
+    println!(
+        "Day {}\n\tPart 1: {}\t({} ms)\n\tPart 2: {}\t({} ms)",
+        day_num,
+        part1,
+        part1_time.as_micros() as f64 / 1000.0,
+        part2,
+        part2_time.as_micros() as f64 / 1000.0
+    );
+}
+
 macro_rules! print_solutions {
     (impl $day:expr => $solution:expr) => {{
-        let input = std::fs::read_to_string(format!("inputs/day{}.txt", $day)).unwrap();
-
-        let (part1, part1_time) = {
-            let start = Instant::now();
-            let part1 = $solution.part1(&input);
-            (part1, start.elapsed())
-        };
-        let (part2, part2_time) = {
-            let start = Instant::now();
-            let part2 = $solution.part2(&input);
-            (part2, start.elapsed())
-        };
-
-        println!(
-            "Day {}\n\tPart 1: {}\t({} ms)\n\tPart 2: {}\t({} ms)",
-            $day,
-            part1,
-            part1_time.as_micros() as f64 / 1000.0,
-            part2,
-            part2_time.as_micros() as f64 / 1000.0
-        );
+        print_solution($day, $solution);
     }};
 
     ($day:expr => $solution:expr;) => {{
@@ -48,14 +52,29 @@ macro_rules! print_solutions {
 }
 
 fn main() {
-    print_solutions!(
-        1 => day1::Day1;
-        2 => day2::Day2;
-        3 => day3::Day3;
-        4 => day4::Day4;
-        5 => day5::Day5;
-        6 => day6::Day6;
-        7 => day7::Day7;
-        8 => day8::Day8;
-    )
+    if let Some(day) = std::env::args().nth(1) {
+        let day = day.parse().unwrap();
+        match day {
+            1 => print_solution(day, day1::Day1),
+            2 => print_solution(day, day2::Day2),
+            3 => print_solution(day, day3::Day3),
+            4 => print_solution(day, day4::Day4),
+            5 => print_solution(day, day5::Day5),
+            6 => print_solution(day, day6::Day6),
+            7 => print_solution(day, day7::Day7),
+            8 => print_solution(day, day8::Day8),
+            _ => panic!("invalid day"),
+        }
+    } else {
+        print_solutions!(
+            1 => day1::Day1;
+            2 => day2::Day2;
+            3 => day3::Day3;
+            4 => day4::Day4;
+            5 => day5::Day5;
+            6 => day6::Day6;
+            7 => day7::Day7;
+            8 => day8::Day8;
+        )
+    }
 }
